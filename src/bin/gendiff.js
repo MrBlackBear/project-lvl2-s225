7776232b1358f1ab09997fa0e286de4adbbb037e
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-03-12 11:48:11 +0300

    FIRST STEP

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+const commander = require('commander');
 
-console.log('11');
+commander
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format');
+
+commander.parse(process.argv);
